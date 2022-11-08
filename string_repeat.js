005ae35d6e2d52dc08050e27c0894f63bcfbbe26
JamesSimel <jameskarinosimel@gmail.com>
2022-11-08 22:06:05 +0300

    Another 8KYU kata...Codewars

---
+++ string_repeat.js
@@ -0,0 +1,14 @@
+// # String repeat challenge in for codewars
+// # Description
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// ```
+//     6, "I"     -> "IIIIII"
+//     5, "Hello" -> "HelloHelloHelloHelloHello"
+// ```
+
+// **Happy coding!...Leave a comment or a star if you like my sol
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
